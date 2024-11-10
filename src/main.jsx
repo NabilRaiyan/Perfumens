25cf3ce38c715e288755a9992e3013bed2c81e3d
NabilRaiyan <rraiyan77@gmail.com>
2024-11-10 22:28:57 +0600

    creating project

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+// finished setting up project with react and tailwind now need to implement react routing
+
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css'
