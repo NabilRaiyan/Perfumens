49c11341324d983c15a23ec25aa20b9ccb419de6
NabilRaiyan <rraiyan77@gmail.com>
2024-11-10 22:30:57 +0600

    adding glb 3d model for perfume bottle

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ const App = () => {
       <h1 className="bg-orange-300 font-serif text-3xl">Hello</h1>
       <h1 className="bg-orange-500 mt-5 font-serif text-3xl">Hello</h1>
       <h1 className="bg-orange-600 font-serif text-3xl">Hello</h1>
-
     </div>
   )
 }
